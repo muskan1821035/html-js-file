bfe1251341be44bac8cdb7fe0bfb2c27e68e2347
muskan1821035 <150235080+muskan1821035@users.noreply.github.com>
2024-03-05 14:01:10 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,109 @@
+// QUESTION 1:
+// a=prompt("enter your serving year")
+// b=parseInt(prompt("enter your salary"))
+// c=b+b*5/100
+// if (a>=5) {
+//     document.write(c)
+// }
+// else{
+// 	document.write(b)
+// }
+
+// QUESTION 2:
+// a=parseInt(prompt("enter your length of rectangle"))
+// b=parseInt(prompt("enter your breadth of rectangle"))
+// if (a==b) {
+// 	document.write("square")
+// }
+// else{
+// 	document.write("not square")
+// }
+// QUESTION 3:
+// a=parseInt(prompt("enter your value"))
+// if(a>=10){
+// 	document.write()
+// }
+// else{
+// 	document.write()
+// }
+// // QUESTION 4:
+// student=[86,54,45,65,87,878]
+// max=0
+// for(i=o;i<student.legth;++i)
+// 	if (max<student[i]) {
+// 		max=student[i]
+// 	}
+// 	document.write(max)
+       // ARRAY
+ // QUESTION 1:   
+ // number=[86,52,67,45,98,23]
+ // count=0
+ // for (i=0;i<number.length;++i) {
+ // 	count=count+number[i]
+ // }
+ // document.write(count)
+
+// QUESTION 2:
+// number=[29,43,55,67,20]
+// value=0
+// for(i=0;i<number.length;++i){
+// 	if (number[i]%2==0)
+// value=value+number[i]
+// }
+// document.write(value)
+// QUESTION 3:
+
+// number=[86,52,67,45,98,23]
+// for(i=0;i<number.length;++i){
+// 	if (number[i]%2==0) {
+// 		document.write(number[i],"number is even<br>")
+// 	}
+// }
+// QUESTION 4:
+
+// arr=[12,92,67,89,90]
+// sum=0
+// count=0
+// for(r=0;r<arr.length;++r){
+// sum=sum+arr[r]
+// count++
+// }
+// avg=sum/count
+// document.write("average is the ",avg)
+
+// a=arsenInt(prompt("enter marks"))
+// if(a<=25){
+// 	document.write("r")
+// }
+
+
+
+function para(){
+	a=document.getElementById("para")
+	if (a.style.display!='none') {
+		a.style.display='none'
+	
+        document.body.style.background='yellow';
+	 document.body.style.color='black'}
+	else{
+		a.style.display='block';
+	document.body.style.background='yellow';
+	document.body.style.color='black'}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
